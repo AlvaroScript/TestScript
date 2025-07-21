d766bd3b5503d7d3297199ec4ebb48d2d8107a71
AlvaroScript <rampe5929@gmail.com>
2025-07-21 16:50:56 +0700

    new

--- Teleport To Elevator.lua
+++ Teleport To Elevator.lua
@@ -1,1 +1,9 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
 
+local targetPosition = Vector3.new(-29.37, 21.07, -61.09)
+
+local character = player.Character
+if character and character:FindFirstChild("HumanoidRootPart") then
+    character.HumanoidRootPart.CFrame = CFrame.new(targetPosition)
+end
