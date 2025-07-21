53c73bcfff03f40d4bf0a30e50180a0df8de3b0d
AlvaroScript <rampe5929@gmail.com>
2025-07-21 16:25:10 +0700

    Delete Normal.lua

--- Normal.lua
+++
@@ -1,8 +0,0 @@
-for i, math.min(#shards,287),12,-1 do
-    local shard = shards[i]
-    if shards then
-        firetouchinterest(shard,hrp,1)
-        task.wait()
-        firetouchinterest(shard,hrp,0)
-    end
-end
