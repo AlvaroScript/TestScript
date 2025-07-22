9517b116b9b1a0f99f0a93f5d3e384dbd8642b09
AlvaroScript <rampe5929@gmail.com>
2025-07-22 15:28:16 +0700

    Update Normal.lua

--- Chapter 1/Normal.lua
+++ Chapter 1/Normal.lua
@@ -1,9 +1,9 @@
-for i, v in pairs(workspace.Shard:GetChildren()) do
-    local part = v
-    local player = game.Players.LocalPlayer.Character
-    local hrp = player:FindFirstChildWhichIsA("BasePart")
+for i, v in pairs(workspace.Shards:GetChildren()) do
+local part = v
+local player = game.Players.LocalPlayer.Character
+local hrp = player: FindFirstChildWhichIsA("BasePart")
 
-    firetouchinterest(part,hrp,0)
-    task.wait()
-    firetouchinterest(part,hrp,1)
+firetouchinterest(part,hrp,0)
+task.wait()
+firetouchinterest(part,hrp,1)
 end
