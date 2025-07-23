066061e3eeda38016aeacdde80adefbe6474d8be
AlvaroScript <rampe5929@gmail.com>
2025-07-23 17:38:14 +0700

    Update Teleport To Elevator.lua

--- Chapter 1/Teleport To Elevator.lua
+++ Chapter 1/Teleport To Elevator.lua
@@ -1,7 +1,7 @@
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
-local targetPosition = Vector3.new(-28, 0, -61)
+local targetPosition = Vector3.new(-28, 5, -61)
 
 local character = player.Character
 if character and character:FindFirstChild("HumanoidRootPart") then
