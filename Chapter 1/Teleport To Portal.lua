ba00eb535fc162c28266636114caffc515b9c034
AlvaroScript <rampe5929@gmail.com>
2025-07-23 17:38:01 +0700

    Update Teleport To Portal.lua

--- Chapter 1/Teleport To Portal.lua
+++ Chapter 1/Teleport To Portal.lua
@@ -1,7 +1,7 @@
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
-local targetPosition = Vector3.new(27, 0, 63)
+local targetPosition = Vector3.new(27, 5, 63)
 local character = player.Character
 if character and character:FindFirstChild("HumanoidRootPart") then
     character.HumanoidRootPart.CFrame = CFrame.new(targetPosition)
