fe7fe0df1436b037b970aec166492e64d0907156
AlvaroScript <rampe5929@gmail.com>
2025-08-12 19:13:48 +0700

    Delete Chapter 1/Teleport To Elevator.lua

--- Chapter 1/Teleport To Elevator.lua
+++
@@ -1,9 +0,0 @@
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
-local targetPosition = Vector3.new(-28, 5, -61)
-
-local character = player.Character
-if character and character:FindFirstChild("HumanoidRootPart") then
-    character.HumanoidRootPart.CFrame = CFrame.new(targetPosition)
-end
